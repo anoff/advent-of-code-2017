5e50e8e8405d49a799bc6cdc3ee8cf7f6265b457
anoff <offenhaeuser@gmail.com>
2017-12-02 14:44:21 +0100

    :star2: feat: day2 part2

--- 2-checksum.py
+++ 2-checksum.py
@@ -13,7 +13,14 @@ if __name__ == '__main__':
     
     input_str = argv[1]
     if len(argv) > 2 and argv[2] == "divisible":
-        print("weee")
+        total_sum = 0
+        for line in input_to_lines(input_str):
+            numbers = line_to_numbers(line)
+            for n in numbers:
+                divisor = [e for e in numbers if e != n and n%e == 0]
+                if divisor:
+                    total_sum += int(n/divisor[0])
+        print("The evenly dividible values sum is:", total_sum)
     else:
         max_ranges = [max(line_to_numbers(line)) - min(line_to_numbers(line)) for line \
             in input_to_lines(input_str)]
